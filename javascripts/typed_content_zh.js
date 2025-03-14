0b593b516fa74b38d7bc638f8f42a917d7cd9f45
RonaldLN <RonaldLN@users.noreply.github.com>
2025-03-14 09:29:45 +0000

    Deployed 592664e with MkDocs version: 1.5.3

---
+++ javascripts/typed_content_zh.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#typed-des', {
+  strings: ['这是一本^1000记录了^100我踩过的坑^500的小手册'],
+  typeSpeed: 50,
+  backSpeed: 0,
+  // cursorChar: '_',
+  smartBackspace: true, // this is a default
+  // fadeOut: true,
+  // shuffle: true,
+  loop: false
+});
