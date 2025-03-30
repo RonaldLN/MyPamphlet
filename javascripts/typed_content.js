3557e8d73b41928227b156f4d8633ac5ab26e4f0
RonaldLN <RonaldLN@users.noreply.github.com>
2025-03-30 07:56:51 +0000

    Deployed a66aeb3 with MkDocs version: 1.5.3

---
+++ javascripts/typed_content.js
@@ -0,0 +1,11 @@
+var description = 'This is a ^1000 s`mall` pa`mphlet` ^500 `<br>` that records^100 the pits^100 I have stepped on.';
+var typed = new Typed('#typed-des', {
+  strings: [description],
+  typeSpeed: 20,
+  backSpeed: 0,
+  cursorChar: '_',
+  smartBackspace: true, // this is a default
+  // fadeOut: true,
+  // shuffle: true,
+  loop: false
+});
